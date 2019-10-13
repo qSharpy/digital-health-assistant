6ca3fce1ea05d8610610db4ffd28168c25d31464
Alin Florin <alin@qoffice.ro>
2019-10-13 21:40:45 +0300

    -

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,8 +5,8 @@ export const process = functions.https.onRequest((request, response) => {
   response.setHeader("Access-Control-Allow-Origin", "*");
   response.setHeader("Access-Control-Allow-Methods", "*");
   response.setHeader("Access-Control-Allow-Headers", "*");
-  const phoneNo: string = request.body.phoneNo;
-  const email: string = request.body.email;
+  /*const phoneNo: string = request.body.phoneNo;
+  const email: string = request.body.email;*/
   if (request.body.text == null || request.body.text.length === 0) {
     response.send({ say: 'Nu am inteles. Repeta.' });
     return;
