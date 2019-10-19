e61b8d63357ce3317cd22a5fc11c93fc8eb7c1ce
Alin Ciu <alin.ciu@endava.com>
2019-10-19 19:26:26 +0300

    -

--- functions/src/accounts.ts
+++ functions/src/accounts.ts
@@ -93,7 +93,7 @@ export const getDoctorAppointments = functions.https.onRequest((request, respons
 });
 
 
-export const getClinicAppointments = functions.https.onRequest((request, response) => {
+export const getMyClinicAppointments = functions.https.onRequest((request, response) => {
     setCorsHeaders(response);
     const userId = request.query.userId;
     const firestore = admin.firestore();
