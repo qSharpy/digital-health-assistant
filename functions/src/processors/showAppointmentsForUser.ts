b6d072af87b78f153e0b38fd3ce7305b308979d7
Marius Oprea <marius.oprea03@gmail.com>
2019-10-19 23:56:08 +0300

    show my appointments treat case when user not logged in

--- functions/src/processors/showAppointmentsForUser.ts
+++ functions/src/processors/showAppointmentsForUser.ts
@@ -1,6 +1,6 @@
 import { Processor, ExecutionResult, ProcessorContext } from "./processor";
-import { Observable } from "rxjs";
-import { map, switchMap, tap } from "rxjs/operators";
+import { Observable, of } from "rxjs";
+import { map, switchMap, tap, catchError } from "rxjs/operators";
 import { getAppointmentsForUser, getAccountDetailsEmailOrPhone } from "../accounts";
 
 export class ShowAppointmentsForUser extends Processor {
@@ -14,10 +14,16 @@ export class ShowAppointmentsForUser extends Processor {
       switchMap(account => getAppointmentsForUser(account.id)),
       map(appointments => ({
         isPositiveAnswer: appointments && appointments.length > 0,
-        dataForReplacing: [appointments.map(x => 
+        dataForReplacing: [appointments.map(x =>
           "<br/> Appointment from " + new Date(x.start_date.seconds * 1000).toLocaleString() + " to " + new Date(x.end_date.seconds * 1000).toLocaleString()).join(";")]
       } as ExecutionResult
-      ))
+      )),
+      catchError((e) => {
+        return of({
+          isPositiveAnswer: false
+        } as ExecutionResult
+        )
+      })
     );
 
     return a;
