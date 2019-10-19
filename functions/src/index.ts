faa14c420c8c2e24c9b9309e7cd5c17c5e8eeae1
Alin Ciu <alin.ciu@endava.com>
2019-10-19 10:45:30 +0300

    -

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,11 @@
-export * from './process';
-export * from './sendEmail';
-export * from './clinics';
-export * from './doctors';
 import * as admin from "firebase-admin";
 import { creds } from "./firebaseServiceAccountKey";
 
 admin.initializeApp({
     credential: admin.credential.cert(creds)
-  });
+});
+
+export * from './process';
+export * from './sendEmail';
+export * from './clinics';
+export * from './doctors';
